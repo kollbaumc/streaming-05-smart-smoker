5f9461ce486752e926669421b65c8327a9ff0b60
kollbaumc <kollbaumc@yahoo.com>
2023-02-16 08:52:29 -0600

    New Listener Commit

--- 02-Food-A_listener.py
+++ 02-Food-A_listener.py
@@ -49,6 +49,8 @@ def main_FoodA(hn: str = "localhost", qn: str = "task_queue"):
         # use the connection to create a communication channel
         channel = connection.channel()
 
+        channel.queue_delete("02-Food-A")
+
         # use the channel to declare a durable queue
         # a durable queue will survive a RabbitMQ server restart
         # and help ensure messages are processed in order
